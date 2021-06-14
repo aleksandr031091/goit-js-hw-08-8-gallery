f6cde6ac58db05bc450baa9100d99c95625411fc
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-06-14 16:10:08 +0300

    end hw-08

--- js/index.js
+++ js/index.js
@@ -1,24 +1,70 @@
 // Разбей задание на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 // Открытие модального окна по клику на элементе галереи.
 // Подмена значения атрибута src элемента img.lightbox__image.
 // Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
 // Очистка значения атрибута src элемента img.lightbox__image.
-// Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение,
-//     мы не видели предыдущее.
+// Это необходимо для того, чтобы при следующем открытии модального окна,
+// пока грузится изображение, мы не видели предыдущее.
 
-/* <li class="gallery__item">
+import dataImages from "./app.js";
+
+const refs = {
+  gallery: document.querySelector(".js-gallery"),
+  lightbox: document.querySelector(".js-lightbox"),
+  ightboxImage: document.querySelector(".lightbox__image"),
+  lightboxOverlay: document.querySelector(".lightbox__overlay"),
+  btnClose: document.querySelector(".lightbox__button"),
+};
+
+const renderMarkup = renderMarcupGallery(dataImages);
+refs.gallery.innerHTML = renderMarkup;
+
+refs.gallery.addEventListener("click", onClickImage);
+refs.btnClose.addEventListener("click", removeClass);
+refs.lightboxOverlay.addEventListener("click", removeClass);
+window.addEventListener("keyup", onPress);
+
+function renderMarcupGallery(galleryImages) {
+  return galleryImages
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
   <a
     class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+    href="${original}"
   >
     <img
       class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</li>; */
+</li>`
+    )
+    .join("");
+}
+
+function onClickImage(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  refs.lightbox.classList.add("is-open");
+  refs.ightboxImage.src = event.target.dataset.source;
+  refs.ightboxImage.alt = event.target.alt;
+}
+
+function removeClass() {
+  refs.lightbox.classList.remove("is-open");
+  refs.ightboxImage.src = "";
+  refs.ightboxImage.alt = "";
+}
+function onPress(event) {
+  if (event.keyCode === 27) {
+    removeClass();
+  }
+}
